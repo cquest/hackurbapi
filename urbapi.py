0e8d860cf5c097439bf14ff3157d2a1523c62fd1
Christian Quest <cquest@openstreetmap.fr>
2017-02-26 11:00:07 +0100

    première mouture fonctionnelle

---
+++ urbapi.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python3
+import falcon
+import psycopg2
+
+class GpuResource(object):
+    def getGpu(self, req, resp):
+        db = psycopg2.connect("dbname=gpu")
+        cur = db.cursor()
+
+        lat = float(req.params.get('lat',48.85))
+        lon = float(req.params.get('lon',2.35))
+        dist = float(req.params.get('dist',100))
+
+        # get a building from the allready partially crowdsourced ones
+        query = cur.mogrify("""SELECT json_build_object('type','FeatureCollection','features',array_agg(geojson::json))::text
+            FROM gpu_all
+            WHERE ST_Intersects(wkb_geometry, ST_Buffer(ST_MakePoint(%s,%s)::geography, %s)::geometry) """,(lon,lat,dist))
+        print(query)
+        cur.execute(query)
+
+        gpu = cur.fetchone()
+
+        resp.status = falcon.HTTP_200
+        resp.set_header('X-Powered-By', 'HackUrbAPI')
+        resp.set_header('Access-Control-Allow-Origin', '*')
+        resp.set_header('Access-Control-Allow-Headers', 'X-Requested-With')
+        resp.body = (gpu[0])
+        db.close()
+
+    def on_get(self, req, resp):
+        self.getGpu(req, resp);
+
+# falcon.API instances are callable WSGI apps
+app = falcon.API()
+
+# Resources are represented by long-lived class instances
+gpu = GpuResource()
+# things will handle all requests to the matching URL path
+app.add_route('/gpu', gpu)
